8bb583cff3ab3981ff0299f0cef4a36505ce1cbd
DeKatel <de.katel@yandex.ru>
2023-02-28 19:00:19 +0300

    ошибка типизации функции-конструктора

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -9,7 +9,7 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
+import stylesе from './Counter.module.css';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
@@ -18,46 +18,90 @@ export function Counter() {
 
   const incrementValue = Number(incrementAmount) || 0;
 
+
+
+
+  interface Obj {
+    deviceType: string;
+    power: number;
+    deviceTitle: string;
+    condition: string;
+    color: string;
+    task: string;
+
+  }
+
+  type SomeConstructor = {
+    new(title: string, power: number, color: string, task: string): Obj;
+  }
+
+  function Device(this: Obj, title: string, power: number, color: string, task: string) {
+
+    this.deviceType = 'electronic';
+    this.power = power;
+    this.deviceTitle = title;
+    this.condition = 'OFF';
+    this.color = color;
+    this.task = task;
+  }
+
+  Device.prototype.switch = function () {
+
+    this.condition = confirm(`Включить ${this.deviceTitle}?`) ? 'ON' : 'OFF';
+  }
+
+  Device.prototype.checkSwitch = function () {
+
+    if (this.condition == 'ON') {
+      console.log(`${this.deviceTitle} включен!  Его мощность ${this.power} Вт`)
+    } else {
+      console.log(`${this.deviceTitle} выключен.`)
+    }
+  }
+
+  const computer = new Device('Компьютер', 500, 'white', 'education');
+
+
   return (
     <div>
-      <div className={styles.row}>
+      <div className={stylesе.row}>
         <button
-          className={styles.button}
+          className={stylesе.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span className={stylesе.value}>{count}</span>
         <button
-          className={styles.button}
+          className={stylesе.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div className={stylesе.row}>
         <input
-          className={styles.textbox}
+          className={stylesе.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
+          className={stylesе.button}
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
+          className={stylesе.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
-          className={styles.button}
+          className={stylesе.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
